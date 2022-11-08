36888c96a8bfea8f411b00d7da406fa42f65cdb0
devihall <devis0306@gmail.com>
2022-11-08 11:48:22 -0500

    changes to hero and homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Hero from "../components/Hero/Hero";
+import Form from "../components/Form/Form";
+
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <Form/>
     </div>
   );
 };
