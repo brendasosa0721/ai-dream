f7ac7d29c9f05ae1953a433d10d362cb40ffa23b
brendasosa0721 <brenda.sosa@gmail.com>
2022-11-08 17:41:49 -0500

    added footer into app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ResponsiveAppBar from "./components/Navigation";
-import Footer from './components/Footer';
+import Footer from './components/Footer/Footer';
 import Home from "./pages/Home";
 import Results from "./pages/Results";
 import Signin from "./pages/Signin";
